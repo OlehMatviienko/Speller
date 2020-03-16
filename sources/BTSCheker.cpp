00d5b9fb855e3174ff540cded2c41ee5a8a021b0
OlehMatviienko <olehmatviienko98@gmail.com>
2020-03-16 12:00:35 +0200

    BTSCheker refactored

--- sources/BTSCheker.cpp
+++ sources/BTSCheker.cpp
@@ -19,32 +19,27 @@ void BTS::add(std::vector<std::string> input) {
     std::mt19937 g(rd());
     std::shuffle(input.begin(), input.end(), g);
     for(const auto& data : input) {
-        // Create node using data from argument list.
+
         node *temp = CreateNode(data);
         node *t = new node;
         t = root;
 
-        // If root is null, assign it to the node created.
+
         if (root == nullptr)
             root = temp;
         else {
-            // Find the position for the new node to be inserted.
             while (t != nullptr) {
                 if (t->data.compare(data) > 0) {
                     if (t->right == nullptr) {
-                        // If current node is NULL then insert the node.
                         t->right = temp;
                         break;
                     }
-                    // Shift pointer to the left.
                     t = t->right;
                 } else if (t->data.compare(data) <= 0) {
                     if (t->left == nullptr) {
-                        // If current node is NULL then insert the node.
                         t->left = temp;
                         break;
                     }
-                    // Shift pointer to the left.
                     t = t->left;
                 }
             }
@@ -61,7 +56,6 @@ void BTS::find(std::string input) {
         AmountOfWords++;
         temp = root;
         bool found= false;
-    // Run the loop untill temp points to a NULL pointer.
             while (temp != nullptr)
             {
                 if (temp->data == s)
